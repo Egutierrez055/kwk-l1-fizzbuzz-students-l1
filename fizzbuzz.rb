4ce607e7372a9e8b3bf38fbb8b676d53716e4115
Egutierrez055 <egutierrez055@gmail.com>
2018-06-06 20:10:29 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,18 @@
+  # If the number is divisible by 3 then it is fizz
+  # If the number is divisible by 5 it is buzz
+  # If the number is divisible by 3 and 5 it is fizzbuzz
+
+def fizzbuzz(int)
+  
+  if int % 3 == 0; int % 5 == 0
+    puts "Fizzbuzz"
+    elsif int % 3 == 0 
+    puts "Fizz"
+    elsif int % 5 == 0 
+    puts "Buzz"
+  else
+    puts "nil"
+  end
+end
+  
+fizzbuzz(50)  
